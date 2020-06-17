cf751580cdb4fc4608ea409d11a97e643366a467
Gabriel Espinel <gabriel.espinel.c@gmail.com>
2020-06-17 07:40:23 -0400

    Adds CSS minification to build

--- www/postcss.config.js
+++ www/postcss.config.js
@@ -2,6 +2,11 @@ module.exports = {
     plugins: [
       require("postcss-import"),
       require("postcss-nested"),
-      require("tailwindcss")
+      require("tailwindcss"),
+
+      ...process.env.NODE_ENV === 'production'
+            ? [require('css-mqpacker')(),
+               require('cssnano')()]
+            : []
     ],
   }
