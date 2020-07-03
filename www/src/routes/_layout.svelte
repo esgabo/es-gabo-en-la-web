372b8fa230fbb97dc98ced0ff4d0ccdd799571a9
Snox <jsmoing@gmail.com>
2020-07-02 20:32:27 -0400

    Modificacion en el header. sustitucion de ondas a elipse en el header.

--- www/src/routes/_layout.svelte
+++ www/src/routes/_layout.svelte
@@ -7,10 +7,15 @@
 </script>
 
 <main class="content-wrapper bg-background-primary font-sans text-copy-primary leading-normal flex flex-col min-h-screen">
-  <header class="text-white gradient">
-    <Nav {segment}/>
-    <WaveTop {segment}/>
-  </header>
+	<div class="bg-background-primary h-12"></div>
+	<div class="shadowesp">
+		<div class="ellipsex">
+			<header class="text-white gradient md:pt-4 md:pb-16 lg:pt-12 lg:pb-32">
+    		<Nav {segment}/>
+    		<WaveTop {segment}/>
+  		</header>
+		</div>
+	</div>
   <div class="flex-grow">
     <slot />
   </div>
