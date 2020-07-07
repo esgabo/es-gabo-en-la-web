4c3a8b91da15f38d9f3445cdd7c640020f778422
Snox <jsmoing@gmail.com>
2020-07-07 12:24:23 -0400

    resto de las paginas con los valores decorativos
    acerca de, contacto y blog.

--- www/src/routes/contacto.svelte
+++ www/src/routes/contacto.svelte
@@ -2,32 +2,28 @@
 	<title>Contacto</title>
 </svelte:head>
 
-<section class="bg-white border-b py-8">
+<section class="bg-clg py-8 flex flex-col items-center">
+	<h1 class="relative mt-8 z-10 rounded-lg bg-white text-3xl font-bold px-2 py-2 text-gray-800 shadow-lg">Encuéntrame en las redes</h1>
 	<div class="container max-w-5xl mx-auto m-8 lg:w-4/6">
-		<h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">Encuéntrame en las redes</h1>
-		<div class="w-full mb-4">	
-			<div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
-		</div>
-	
-		<div class="flex flex-wrap">
-			<div class="w-4/6 sm:w-1/2 p-6">
+		<div class="flex flex-wrap sm:flex-no-wrap ">
+			<div class="w-4/6 sm:w-1/2 p-6 ml-4 flex flex-wrap bg-white shadow-md rounded-lg">
 				<h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">Estoy en YouTube</h3>
 				<p class="text-gray-600 mb-8">Suscribete a mi canal de YouTube para que no te pierdas ninguno de los videos donde hablo de temas de programación y desarrollo de software<br /><br />
-				
+
 				Enlace: <a class="text-orange-500 underline" href="https://www.youtube.com/channel/UCg5oehh0kF-XBfuuqgGOqsA">Ir al Canal</a></p>
-				
+
 			</div>
-			<div class="w-full sm:w-1/2 p-6">
+			<div class="w-full sm:w-1/2 p-6 shadowf">
 				<img class="w-full sm:h-64 mx-auto" src="youtube.svg" alt="youtube"/>
 			</div>
 		</div>
-		
-		
-		<div class="flex flex-wrap flex-col-reverse sm:flex-row">	
-			<div class="w-full sm:w-1/2 p-6 mt-6">
-				<img class="w-5/6 sm:h-64 mx-auto" src="ig.svg" alt="instagram"/>
+
+
+		<div class="flex flex-wrap flex-col-reverse sm:flex-no-wrap sm:flex-row">
+			<div class="w-full sm:w-1/2 p-6 mt-6 ">
+				<img class="w-5/6 sm:h-64 mx-auto shadowf" src="ig.svg" alt="instagram"/>
 			</div>
-			<div class="w-4/6 sm:w-1/2 p-6 mt-6">
+			<div class="w-4/6 sm:w-1/2 p-6 mt-6 mr-4 ml-4 flex flex-wrap bg-white shadow-md rounded-lg">
 				<div class="align-middle">
 					<h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">También tengo Instagram</h3>
 					<p class="text-gray-600 mb-8">Puedes seguirme en Instagram donde constantemente publico contenido y tips de programación<br /><br />
@@ -36,15 +32,15 @@
 			</div>
 		</div>
 
-		<div class="flex flex-wrap">
-			<div class="w-4/6 sm:w-1/2 p-6">
+		<div class="flex flex-wrap sm:flex-no-wrap mt-6">
+			<div class="w-4/6 sm:w-1/2 p-6 ml-4 bg-white shadow-md rounded-lg">
 				<h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">Claro que tengo LinkedIn</h3>
 				<p class="text-gray-600 mb-8">Puedes encontrar mi perfil de LinkedIn y enviarme una solicitud para conectar<br /><br />
-				
+
 				Enlace: <a class="text-orange-500 underline" href="https://www.linkedin.com/in/ilgespinel/">Ver perfil</a></p>
-				
+
 			</div>
-			<div class="w-full sm:w-1/2 p-6">
+			<div class="w-full sm:w-1/2 p-6 shadowf">
 				<img class="w-full sm:h-64 mx-auto" src="linkedin.svg" alt="linkedin"/>
 			</div>
 		</div>
