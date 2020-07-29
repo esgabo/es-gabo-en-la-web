1ca739c9f12af87d3ddddfe8733f2974abec6224
Gabriel Espinel <gabriel.espinel.c@gmail.com>
2020-07-29 11:44:18 -0400

    Installs JSON rollup plugin

--- www/rollup.config.js
+++ www/rollup.config.js
@@ -3,6 +3,7 @@ import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 import svelte from 'rollup-plugin-svelte';
 import babel from '@rollup/plugin-babel';
+import json from '@rollup/plugin-json';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
@@ -46,7 +47,8 @@ export default {
 		output: config.client.output(),
 		plugins: [
 			markdown(mdConverter),
-			glob(),
+      glob(),
+      json(),
 			replace({
 				'process.browser': true,
 				'process.env.NODE_ENV': JSON.stringify(mode)
@@ -95,6 +97,7 @@ export default {
 		plugins: [
 			markdown(mdConverter),
 			glob(),
+      json(),
 			replace({
 				'process.browser': false,
 				'process.env.NODE_ENV': JSON.stringify(mode)
